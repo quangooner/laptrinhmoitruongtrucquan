1156b37fa5a50ab03f7c2d808c605e69f3958ae2
quangooner <55080280+quangooner@users.noreply.github.com>
2020-04-10 18:39:37 +0700

    Add files via upload

---
+++ repos/PhuongTrinhBac1/PhuongTrinhBac1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PhuongTrinhBac1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************Giai va bien luan ptb1*****************");
+            Console.Write("Nhap a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Nhap b:");
+            int b = int.Parse(Console.ReadLine());
+            if (a == 0)
+                if (b == 0)
+                    Console.WriteLine("Phuong Trinh co vo so nghiem");
+            else
+                    Console.WriteLine("Phuong Trinh vo  nghiem");
+            else
+            {
+                int ketqua = -b / a;
+                Console.WriteLine("Phuong Trinh co  mot nghiem x ={0}",ketqua.ToString());
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
