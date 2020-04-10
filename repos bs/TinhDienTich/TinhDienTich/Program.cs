b6e54d0d9fa2254185ea29466b14d27e59de10d4
quangooner <55080280+quangooner@users.noreply.github.com>
2020-04-10 18:43:15 +0700

    Add files via upload

---
+++ repos bs/TinhDienTich/TinhDienTich/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TinhDienTich
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****************************Tinh Chu VI & DIen Tich ****************************");
+            //OOP Tính diện tích và chu vi của hình chữ nhật
+            Console.Write("Nhap vao chieu dai: ");
+            double d = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap vao chieu rong: ");
+            double r = Convert.ToInt32(Console.ReadLine());
+            hcn tdt = new hcn();
+            hcn tcv = new hcn();
+            Console.WriteLine("Chu vi = {0}", tcv.cv(d, r));
+            Console.WriteLine("Dien tich = {0}", tdt.dt(d, r));
+            
+        }
+
+        public class hcn
+        {
+            //public double dai;
+            //public double rong;
+            public double dt(double dai, double rong)
+            {
+                return dai * rong;
+            }
+            public double cv(double dai, double rong)
+            {
+                return (dai + rong) * 2;
+            }
+        }
+    }
+}
